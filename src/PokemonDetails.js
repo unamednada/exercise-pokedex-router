dac86d30bde9a86cd0f3788cb547f550eede3224
unamednada <dias.gustavo.dias@gmail.com>
2021-09-14 13:33:40 -0300

    Finish adding info to more details. Add some style

--- src/PokemonDetails.js
+++ src/PokemonDetails.js
@@ -7,9 +7,27 @@ class PokemonDetails extends React.Component {
     const { id: curPkm } = this.props.match.params;
     const { pokemons } = this.props;
     const pokemon = pokemons.find(({ id }) => id === +curPkm);
+    const { name, summary, foundAt } = pokemon;
     return (
-      <div className="pokedex">
-        <Pokemon pokemon={pokemon} />
+      <div>
+        <div className="pokedex">
+          <Pokemon pokemon={pokemon} />
+        </div>
+        <div>
+          <h2>Summary</h2>
+          <p>{summary}</p>
+        </div>
+        <div>
+          <h2>{name} game locations</h2>
+          <div className="locations-container">
+            {foundAt.map(({ location, map }) => (
+              <div>
+                <h3>{location}</h3>
+                <img src={map} alt="" />
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     )
   }
