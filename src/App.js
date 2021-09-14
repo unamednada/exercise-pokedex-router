0aa43e6bef552ad8252cfc564163f5b0be78d9fb
unamednada <dias.gustavo.dias@gmail.com>
2021-09-14 13:54:40 -0300

    Create about component and add it to route

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import './App.css';
 import pokemons from './data';
 import Pokedex from './Pokedex';
-import PokemonDetails from './PokemonDetails';      
+import PokemonDetails from './PokemonDetails';
+import About from './About'; 
 
 function App() {
   return (
     <div className="App">
       <h1>Pokedex</h1>
       <BrowserRouter>
+        <Link to="/">Home</Link>
         <Switch>
           <Route path="/pokemon-details/:id" render={(props) => <PokemonDetails {...props} pokemons={pokemons} />} />
+          <Route path="/about" component={About} />
           <Route path="/" render={() => <Pokedex pokemons={pokemons} /> } />
         </Switch>
       </BrowserRouter>
