264853c83b789276f3cbbd459840be113f5bb666
unamednada <dias.gustavo.dias@gmail.com>
2021-09-14 12:39:43 -0300

    Add pokemon to pókemondetails

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import pokemons from './data';
 import Pokedex from './Pokedex';
-import PokemonDetails from './PokemonDetails';
+import PokemonDetails from './PokemonDetails';      
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Switch>
-          <Route path="/pokemon-details/:id" render={(props) => <PokemonDetails {...props} />} />
+          <Route path="/pokemon-details/:id" render={(props) => <PokemonDetails {...props} pokemons={pokemons} />} />
           <Route path="/" render={() => <Pokedex pokemons={pokemons} /> } />
         </Switch>
       </BrowserRouter>
--- src/PokemonDetails.js
+++ src/PokemonDetails.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Pokemon from './Pokemon';
+import './pokedex.css';
 
 class PokemonDetails extends React.Component {
   render() {
+    const { id: curPkm } = this.props.match.params;
+    const { pokemons } = this.props;
+    const pokemon = pokemons.find(({ id }) => id === +curPkm);
     return (
-      <h1>Pikachu</h1>
+      <div className="pokedex">
+        <h1>{pokemon.name}</h1>
+        <Pokemon pokemon={pokemon} />
+      </div>
     )
   }
 }
