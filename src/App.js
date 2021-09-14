578ff3c9856651e8948908b104f98dcc664c9b69
unamednada <dias.gustavo.dias@gmail.com>
2021-09-14 12:21:56 -0300

    Create boilerplate for pokemondetails and add route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import pokemons from './data';
 import Pokedex from './Pokedex';
+import PokemonDetails from './PokemonDetails';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Route path="/" render={() => <Pokedex pokemons={pokemons} /> } />
+        <Switch>
+          <Route path="/pokemon-details/:id" render={(props) => <PokemonDetails {...props} />} />
+          <Route path="/" render={() => <Pokedex pokemons={pokemons} /> } />
+        </Switch>
       </BrowserRouter>
     </div>
   );
---
+++ src/PokemonDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class PokemonDetails extends React.Component {
+  render() {
+    return (
+      <h1>Pikachu</h1>
+    )
+  }
+}
+
+export default PokemonDetails;
